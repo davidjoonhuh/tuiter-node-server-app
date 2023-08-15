bceabd57cdd77e24afb624ed36c4d7d75ea26640
davidjoonhuh <davidjo.huh@gmail.com>
2023-08-15 03:55:33 -0400

    finish a5 render bs

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ import UserController from "./users/users-controller.js";
 import TuitsController from './controllers/tuits/tuits-controller.js';
 import AuthController from './users/auth-controller.js';
 import session from "express-session";
+import "dotenv/config";
 
 const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL
     })
 );
 const sessionOptions = {
@@ -18,13 +19,18 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
-app.use(
-    session(sessionOptions)
-);
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
-const port = process.env.PORT || 4000;
 AuthController(app);
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
