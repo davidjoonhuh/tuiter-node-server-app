ce2ff84a0c47bf321d8a2acf79b89232b4e5015d
davidjoonhuh <davidjo.huh@gmail.com>
2023-08-15 17:40:48 -0400

    init a6

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ import AuthController from './users/auth-controller.js';
 import session from "express-session";
 import "dotenv/config";
 
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+console.log(CONNECTION_STRING)
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(
     cors({
